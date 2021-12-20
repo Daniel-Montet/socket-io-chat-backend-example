9a030bebc43dedf3db0771c6239641f34a054e79
Daniel Montet <dmontetproff@gmail.com>
2021-12-20 16:41:04 +0300

    Added cors configs

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,10 @@ import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
+
 // express configs
-let whitelist = [
-	"http://localhost:3000",
-	"ws://localhost:3000",
-]
+let whitelist = ["http://localhost:3000"]
 let corsOptions = {
 	origin: function (origin: any, callback: any) {
 		if (whitelist.indexOf(origin) !== -1) {
@@ -21,6 +19,7 @@ let corsOptions = {
 		}
 	}
 }
+
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
@@ -31,7 +30,11 @@ io.on("connection", (socket) => {
 	console.info("a user connected");
 	socket.on('disconnect', () => {
 		console.error('user disconnected');
-	})
+	});
+
+	socket.on('chat message', (msg) => {
+		console.log('message: ' + msg);
+	});
 })
 
 server.listen(5000, () => {
